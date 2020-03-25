9a95ee2114d9463359c761d56eb577726c05b424
Kevin Schanz <kschanz@imobile3.com>
2020-03-25 18:17:23 -0400

    [Android] Task 003: Remove product list UI elements

--- Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
@@ -4,24 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.imobile3.groovypayments.R;
 import com.imobile3.groovypayments.data.model.Product;
-import com.imobile3.groovypayments.rules.ProductRules;
 import com.imobile3.groovypayments.utils.StateListHelper;
 
 import org.jetbrains.annotations.NotNull;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class ProductListAdapter
         extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
@@ -55,24 +51,10 @@ public class ProductListAdapter
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Product item = mItems.get(position);
-        ProductRules rules = new ProductRules(item);
 
-        // Primary list item container pressed / default state.
-        holder.container.setBackground(
-                StateListHelper.getBgColorSelector(mContext, rules.getColor().colorRes));
-
-        // Configure the icon and background circle.
-        holder.icon.setImageResource(rules.getIcon().drawableRes);
-        holder.icon.setBackground(
-                ContextCompat.getDrawable(mContext, rules.getColor().colorRes));
-
-        // Configure label and description.
         holder.label.setText(item.getName());
         holder.label.setTextColor(
                 StateListHelper.getTextColorSelector(mContext, R.color.black_space));
-        holder.description.setText(rules.getDescription(Locale.getDefault()));
-        holder.description.setTextColor(
-                StateListHelper.getTextColorSelector(mContext, R.color.gray_down_pour));
     }
 
     @Override
@@ -82,15 +64,12 @@ public class ProductListAdapter
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ViewGroup container;
-        ImageView icon;
-        TextView label, description;
+        TextView label;
 
         ViewHolder(View itemView) {
             super(itemView);
             container = itemView.findViewById(R.id.container);
-            icon = itemView.findViewById(R.id.icon);
             label = itemView.findViewById(R.id.label);
-            description = itemView.findViewById(R.id.description);
             container.setOnClickListener(this);
         }
 
